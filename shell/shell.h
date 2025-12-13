02330d022258ac2d5d1ca5d2c03fdd73bfceaacb
danko1122q <davaniko1122@gmail.com>
2025-12-13 20:28:54 +0700

    modified:   shell/shell.h

--- shell/shell.h
+++ shell/shell.h
@@ -1,13 +1,7 @@
-#ifndef KEYBOARD_H
-#define KEYBOARD_H
+#ifndef SHELL_H
+#define SHELL_H
 
-#include <stdint.h>
-
-void keyboard_init(void);
-char keyboard_getchar(void);
-char keyboard_getchar_with_ctrl(void);
-int keyboard_has_input(void);
-int keyboard_ctrl_pressed(void);
-void keyboard_readline(char *buffer, int max_len);
+void shell_init(void);
+void shell_run(void);
 
 #endif
